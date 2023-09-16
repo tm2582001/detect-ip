7fff3b688b87ddf1d0d11d930cee31288cf8ebab
Tushar Mehta <tsrmehta@gmail.com>
2023-09-16 14:09:52 +0530

    added new logic to detect ipv6

--- index.js
+++ index.js
@@ -4,19 +4,27 @@ const app = express();
 app.set('trust proxy', true);
 
 app.get("/",(req,res)=>{
-    console.log(req.headers);
-    var forwardedIpsStr = req.header('x-forwarded-for');
-    var IP = '';
+    // console.log(req.headers);
+    // var forwardedIpsStr = req.header('x-forwarded-for');
+    // var IP = '';
  
-    if (forwardedIpsStr) {
-       IP = forwardedIps = forwardedIpsStr.split(',')[0];  
-    }
-    console.log(req.ip);
-    res.json({Ip:req.ip, IPV6: IP});
+    // if (forwardedIpsStr) {
+    //    IP = forwardedIps = forwardedIpsStr.split(',')[0];  
+    // }
+    console.log(req.socket.remoteAddres);
+    res.json({Ip:req.ip, IPV6: req.socket.remoteAddres});
 })
 
 const port = process.env.PORT || 8080
 
-app.listen(port,()=>{
+
+const listenServer = () => {
     console.log(`running device on ${port}`)
-})
+    console.log('App running on');
+};
+
+// Use IPv4 address to listen on IPv4
+app.listen(port, listenServer);
+
+// Use IPv6 address to listen on IPv6.
+// app.listen(port, '2401:4900:169e:80c3:11e9:63ba:1f81:d224', listenServer);
