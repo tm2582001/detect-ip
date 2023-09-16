f7f507d8da00c936b2f54d4e3924884a777483d9
Tushar Mehta <tsrmehta@gmail.com>
2023-09-16 13:58:54 +0530

    added logic to detect ipv6

--- index.js
+++ index.js
@@ -4,8 +4,15 @@ const app = express();
 app.set('trust proxy', true);
 
 app.get("/",(req,res)=>{
+    console.log(req.headers);
+    var forwardedIpsStr = req.header('x-forwarded-for');
+    var IP = '';
+ 
+    if (forwardedIpsStr) {
+       IP = forwardedIps = forwardedIpsStr.split(',')[0];  
+    }
     console.log(req.ip);
-    res.json({Ip:req.ip});
+    res.json({Ip:req.ip, IPV6: IP});
 })
 
 const port = process.env.PORT || 8080
